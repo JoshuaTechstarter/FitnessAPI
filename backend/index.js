2f63c8bd186415ad8b1b254c11cf93db5282acac
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-04-02 10:47:07 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fs = require("fs")
+const path = require("path");
 const app = express();
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "frontend")))
 
 
 
