32dd7398a6ea356db133e13ef288acc95a6c26c8
WeisongQi <qiweisong@outlook.com>
2025-04-02 12:13:26 +0200

    fix

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const { error } = require("console");
 const express = require("express");
 const fs = require("fs")
 const path = require("path");
-const cors = require("cors");
+//const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(cors({
-    origin: "http://localhost:5050"
-}))
+// app.use(cors());
+// app.use(cors({
+//     origin: "http://localhost:5050"
+// }))
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "frontend")))
